b799e1b3452bbd355c8025e5c9e2d18ba5bdce59
Robert Weekes <weekes.robert@gmail.com>
2026-04-06 14:05:01 -0700

    removed legacy CSRF/XSRF runtime calls from backend/routes/index.js

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -95,15 +95,3 @@ module.exports = router;
 
 
 // module.exports = router;
-
-
-// Example fetch request:
-
-// fetch("/api/test", {
-//   method: "POST",
-//   headers: {
-//     "Content-Type": "application/json",
-//     "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
-//   },
-//   body: JSON.stringify({ hello: "world" })
-// }).then(res => res.json()).then(data => console.log(data));
